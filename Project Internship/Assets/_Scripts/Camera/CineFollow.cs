aab541c5379606f93751e5fd4b3e430f15fcda0b
HakeemAzman <nurhakeemazman@gmail.com>
2019-06-17 17:33:10 +0800

    Camera Follows Both Character
    Camera Follow both the Thief and Gladiator. Need to tweak the camera a bit now cause it looks a little wonky. Need to add more satisfaction when changing between characters!

--- Project Internship/Assets/_Scripts/Camera/CineFollow.cs
+++ Project Internship/Assets/_Scripts/Camera/CineFollow.cs
@@ -5,16 +5,14 @@ using Cinemachine;
 
 public class CineFollow : MonoBehaviour
 {
-    public GameObject Player_Follow;
+    [SerializeField] GameObject Player_Follow;
 
     // Update is called once per frame
     void Update()
     {
-        if(Player_Follow == null)
-        {
-            Player_Follow = GameObject.FindWithTag("Player");
-        }
-
         this.gameObject.GetComponent<CinemachineVirtualCamera>().Follow = Player_Follow.transform;
+
+        Player_Follow = GameObject.FindWithTag("Player");
+
     }
 }
--- Project Internship/Assets/_Scripts/Core/PlayerSwitch.cs
+++ Project Internship/Assets/_Scripts/Core/PlayerSwitch.cs
@@ -11,12 +11,18 @@ public class PlayerSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (Input.GetButtonDown("Switch"))
         {
             if (isThief)
+            {
+                Player_Gladiator.transform.position = Player_Thief.transform.position;
                 isThief = false;
+            }
             else
+            {
+                Player_Thief.transform.position = Player_Gladiator.transform.position;
                 isThief = true;
+            }          
         }
 
         Characters();
@@ -24,12 +30,12 @@ public class PlayerSwitch : MonoBehaviour
 
     void Characters()
     {
-        if (isThief)
+        if (isThief) //Thief
         {
             Player_Gladiator.SetActive(false);
             Player_Thief.SetActive(true);
         }
-        else
+        else //Gladiator
         {
             Player_Thief.SetActive(false);
             Player_Gladiator.SetActive(true);
