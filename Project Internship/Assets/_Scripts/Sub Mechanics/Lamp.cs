7b02b7dcef08951045d9ca0655cbd57242fbf8f8
HakeemAzman <nurhakeemazman@gmail.com>
2019-06-20 13:25:38 +0800

    First Cutscene Added!
    CUTSCENE 1
    After collecting the lamp,
    camera shakes
    rocks falls on pillars
    fire turns off
    door opens
    light penetrates the whole cave

--- Project Internship/Assets/_Scripts/Sub Mechanics/Lamp.cs
+++ Project Internship/Assets/_Scripts/Sub Mechanics/Lamp.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class Lamp : MonoBehaviour
 {
+    #region Variables
     [SerializeField] PlayerSwitch ps;
+
+    [SerializeField] GameObject Start_Cutscene, lamp;
+
+    [SerializeField] Light Spotlight_lamp;
+
+    [SerializeField] Animator rocks, hidden_door;
+
+    [SerializeField] string rockFall, hiddenDoorMove;
+    float r = 255f, g = 128f, b = 124f;
+    //FF807C
+    #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             ps.gameObject.GetComponent<PlayerSwitch>().canSwitch = true;
-            Destroy(this.gameObject);
+            StartCoroutine(StartFirstCutscene());
+
         }
+    }
+
+    IEnumerator StartFirstCutscene()
+    {
+        Start_Cutscene.SetActive(true);
+        lamp.GetComponent<MeshRenderer>().enabled = false;
+        Spotlight_lamp.gameObject.GetComponent<Light>().color = Color.red;
+
+        yield return new WaitForSeconds(2.5f);
+
+        rocks.gameObject.GetComponent<Animator>().Play(rockFall);
+
+        yield return new WaitForSeconds(2.5f);
+
+        hidden_door.gameObject.GetComponent<Animator>().Play(hiddenDoorMove);
+
+        yield return new WaitForSeconds(2f);
+        
+        Destroy(this.gameObject);
     }
 }
