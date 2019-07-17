67dd632e558081e695e25efb5bec4d61246d5948
Hakeem Azman <nurhakeemazman@gmail.com>
2019-07-18 00:51:48 +0800

    Major Updates
    - Collectables
    - Checkpoint (Respawn Player to the last checkpoint)
    - Enemies
    - Enemy Patrol depending on the length of the platform
    - Lightmapping
    - Added Player Animation
    - Level Balancing

--- Project Internship/Assets/_Scripts/Core/Enemy.cs
+++ Project Internship/Assets/_Scripts/Core/Enemy.cs
@@ -4,10 +4,43 @@ using UnityEngine;
 
 public class Enemy : Actor
 {
+    [Header("Moving Settings")]
+    public bool isMovable;
+    public float Movement_Speed, Ray_Distance;
+    
+    bool isRight = true;
+
+    [SerializeField] Transform Player_Pos;
+    [SerializeField] LayerMask Player_LayerMask;
+
     // Start is called before the first frame update
     void Start()
     {
         Init();
+    }
+
+    private void Update()
+    {
+        if(isMovable)
+        {
+            transform.Translate(Vector3.forward * Movement_Speed * Time.deltaTime);
+
+            bool isGroundDetected = Physics2D.OverlapCircle(Player_Pos.position, 0.1f, Player_LayerMask);
+
+            if(!isGroundDetected)
+            {
+                if (isRight)
+                {
+                    transform.localRotation = Quaternion.Euler(0, 90, 0);
+                    isRight = false;
+                }
+                else
+                {
+                    transform.localRotation = Quaternion.Euler(0, -90, 0);
+                    isRight = true;
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D enter)
